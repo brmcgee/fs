fd84a6c169c234d0be5c7abea618754c4ec97a0f
Brian McGee <92900596+brmcgee@users.noreply.github.com>
2024-02-23 21:56:09 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -14,37 +14,45 @@ function elem (str) {
 function displayAll () {
     f_blogs.forEach(blog => {
     
-        let html = ` 
-            <div class="card" id="blog${blog.id}">
-                <div class="card-img-top">
-                    <img src="${blog.img}" alt="">
+        if (blog.category.trim() === "Military") {
+
+
+            let html = ` 
+                <div class="card" id="blog${blog.id}">
+                    <div class="card-img-top">
+                        <img src="${blog.img}" alt="">
+                    </div>
+                    <div class="card-body">
+                        <div class="card-title">${blog.title}</div>
+                        <div class="card-body">${blog.body}</div>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <div class="card-title">${blog.title}</div>
-                    <div class="card-body">${blog.body}</div>
-                </div>
-            </div>
-        `;
-        viewContainer.innerHTML += html;
+            `;
+            viewContainer.innerHTML += html;
+            
+            f_comment.forEach(comment => {
+                commentHTML = '';
+                if (comment.post_id == blog.id) {
+                    commentHTML +=  `
+                    <div class="comment comment${blog.id}">
+                        <div class="comment-img-top">
+                            <img src="${comment.auth_avatar}" alt="">
+                        </div>
+                        <div class="comment-body">
+                            <div class="comment-title">${comment.author}</div>
+                            <div class="comment-body">${comment.comment}</div>
+                        </div>
+                    </div>        
+                    `;
+                }
+                viewContainer.innerHTML += commentHTML;
+            });  
+            
+        }
+
+
+        });
         
-        f_comment.forEach(comment => {
-            commentHTML = '';
-            if (comment.post_id == blog.id) {
-                commentHTML +=  `
-                <div class="comment comment${blog.id}">
-                    <div class="comment-img-top">
-                        <img src="${comment.auth_avatar}" alt="">
-                    </div>
-                    <div class="comment-body">
-                        <div class="comment-title">${comment.author}</div>
-                        <div class="comment-body">${comment.comment}</div>
-                    </div>
-                </div>        
-                `;
-            }
-            viewContainer.innerHTML += commentHTML;
-        });       
-    });
     
 };
 
